9a043e43274bc43dc2597e22db72671913e69e19
damien <dalien2611@hotmail.fr>
2014-04-18 23:23:21 -0400

    test

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
 var express = require('express');
-var arduino = require('duino'),
-  board = new arduino.Board();
-
+var arduino = require('duino')
+var board = new arduino.Board({
+  debug: true
+});
 var app = express();
 
 
 
 
-board.on('ready', function(){
 
-  app.get('/on', function(req, res) {
+
+
+board.on('connected', function(){
+  console.log('connected serial');
+
+
+    console.log('board ready');
+
+    app.get('/on', function(req, res) {
 //  res.send([{name:'wine1'}, {name:'wine2'}]);
 
-    var toSend = 1;
-    board.write(parseInt(toSend));
-    console.log(toSend);
-  });
+      var toSend = 1;
+      board.write("1111111111111111111");
+      console.log(toSend);
+      res.end();
+    });
 
-  app.get('/off', function(req, res) {
+    app.get('/off', function(req, res) {
 //  res.send([{name:'wine1'}, {name:'wine2'}]);
 
-    var toSend = 0;
-    board.write(parseInt(toSend));
+      var toSend = 0;
+      board.write("0000000000000000000");
 
-    console.log(toSend);
-  });
+      console.log(toSend.toString());
+      res.end();
+
+    });
 
 });
 
-board.on('data', function(m){
-  console.log('received data serial arduino : ');
-  console.log(m);
-});
+    board.on('message', function(m){
+      console.log('received data serial arduino : ');
+      console.log(m);
+    });
+
+
+
+
+
+//});
+
+
 
 app.listen(3000);
 console.log('Listening on port 3000...');
