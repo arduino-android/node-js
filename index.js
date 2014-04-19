712caa9ca0a850392f90abcfebb00cf9dbd775cb
damien <dalien2611@hotmail.fr>
2014-04-18 21:32:05 -0400

    test

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-module.exports = function () {
-  return 'Hello, world';
-};
+var express = require('express');
+var arduino = require('duino'),
+  board = new arduino.Board();
+
+var app = express();
+
+
+
+
+board.on('ready', function(){
+
+  app.get('/on', function(req, res) {
+//  res.send([{name:'wine1'}, {name:'wine2'}]);
+
+    var toSend = 1;
+    board.write(parseInt(toSend));
+    console.log(toSend);
+  });
+
+  app.get('/off', function(req, res) {
+//  res.send([{name:'wine1'}, {name:'wine2'}]);
+
+    var toSend = 0;
+    board.write(parseInt(toSend));
+
+    console.log(toSend);
+  });
+
+});
+
+board.on('data', function(m){
+  console.log('received data serial arduino : ');
+  console.log(m);
+});
+
+app.listen(3000);
+console.log('Listening on port 3000...');
